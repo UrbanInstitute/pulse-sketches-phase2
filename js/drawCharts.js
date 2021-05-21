58180c6702d594b4d9a1df46197ab45a74ca7cf0
JoElla Carman <jstraley@gmail.com>
2021-05-21 11:10:05 -0400

    adds new data, new phase 3.1 line, and data note

--- js/drawCharts.js
+++ js/drawCharts.js
@@ -9,13 +9,11 @@ var margin = {
 };
 
 var width,
-    height,
-    myBandwidth, //bc if you don't use scaleBand you need to make bands
-    numWeeks;
+    height;
 
-var x = d3.scaleTime() //d3.scaleBand()
-    // .paddingInner(0.2)
-    // .paddingOuter(0.5);
+var x = d3.scaleBand()
+    .paddingInner(0.2)
+    .paddingOuter(0.5);
 
 var y = d3.scaleLinear()
     .domain([0,1]);
@@ -26,6 +24,10 @@ var parseTime = d3.timeParse('%m/%d/%y');
 
 var selected_indicator = "food_insufficient";
 var selected_geo = "US";
+
+var div = d3.select("body").append("div")
+  .attr("class", "chart-tooltip")
+  .style("opacity", 0);
 
 // dummy data so that elements are entered on the National graph on load
 // as placeholders for the state/MSA-specific points
@@ -85,8 +87,6 @@ function drawGraphic(containerWidth) {
     height = (containerWidth < 450) ? (width * 0.66) - margin.top - margin.bottom : 230;
     num_ticks = (containerWidth < 400) ? 5 : 10;
 
-    myBandwidth = width / numWeeks
-
     x.rangeRound([0, width]);
     y.rangeRound([height, 0]);
 
@@ -108,12 +108,16 @@ function drawGraphic(containerWidth) {
     if (pymChild) {
         pymChild.sendHeight();
     }
+
+
+    d3.selectAll('.tick line').moveToFront()
+    // d3.select('g.axis.y-axis > g:nth-child(2) > line').moveToFront()
 }
 
 // data can be found here: https://ui-census-pulse-survey.s3.amazonaws.com/phase2_all_to_current_week.csv
 // NOTE: if the en dashes in the date ranges aren't showing up properly,
 //       open the csv file in Sublime and go to File > Save with Encoding > UTF-8
-d3.csv("data/dummy.csv", function(d) {
+d3.csv("data/phase2_all_to_current_week_feature.csv", function(d) {
     return {
         metric: d.metric,
         race_var: d.race_var,
@@ -130,16 +134,15 @@ d3.csv("data/dummy.csv", function(d) {
     };
 }, function(error, data) {
 
+    data = data.filter(function(d){
+      return d.date_int !== 'NA' //Alena marked variables removed from 3.1 as 'NA'
+    })
+
     pulseData = data;
 
-    var unique_weeks = d3.map(data, function(d) {return d.date_int;}).keys();
+    var unique_weeks = d3.map(data, function(d) { return d.date_int;}).keys();
 
-    var beginnings_of_weeks_ranges = unique_weeks.map(function(d){ return parseTime(d.split('–')[0]) });
-
-    x.domain( d3.extent(beginnings_of_weeks_ranges) )
-
-    numWeeks = beginnings_of_weeks_ranges.length;
-    // x.domain(unique_weeks);
+    x.domain( unique_weeks )
 
     unique_weeks.forEach(function(w) {
         dummy_obs.date_int = w;
@@ -187,17 +190,12 @@ function setupChart(race) {
 
 
     // add axes
-    g.append("g")
-        .attr("class", "axis y-axis")
-        .call(d3.axisLeft(y).tickFormat(PCTFORMAT).tickSize(-width).ticks(num_ticks));
+
 
     g.append("g")
         .attr("class", "axis x-axis")
         .attr("transform", "translate(0," + height + ")")
-        .call(d3.axisBottom(x)
-          // .tickSizeOuter(0)
-          .tickFormat( function(d){ debugger })//d3.timeFormat('%m/%d/%y'))
-        )
+        .call(d3.axisBottom(x).tickSizeOuter(0))
 
         g.select(".x-axis")
             .selectAll(".tick")
@@ -206,11 +204,37 @@ function setupChart(race) {
             });
 
     d3.selectAll(".x-axis .tick text")
-        // .call(wrap, myBandwidth )
+        .call(wrap, x.bandwidth())
         .attr("transform", "translate(0,5)"); // move labels down to make space for longer ticks
 
     d3.selectAll('.tick.t0 line').attr('y2', 8) // make major tick lines longer
     d3.selectAll('.tick line').attr('y1', 1) // start minor ticks below x-axis to avoid overlap
+
+    //add mouseover to minor tick marks
+    d3.selectAll('.tick.t1, .tick.t2')
+      .append('rect')
+      .attr('width', x.bandwidth())
+      .attr('height', 20)
+      .attr('fill', '#FFFFFF')
+      .attr('y', -10 )
+      .attr('x', -x.bandwidth() / 2)
+      .on('mouseover', function(d){
+        var tickMark = $(this),
+          tooltipWidth = $('.chart-tooltip').outerWidth();
+        div.transition()
+            .style('opacity', .98);
+        div.html(d)
+            .style('left', ( (tickMark.offset().left + x.bandwidth() /2 ) - tooltipWidth / 2) + "px")
+            .style('top', tickMark.offset().top + 16 + "px");
+      })
+      .on('mouseout', function(){
+        div.transition()
+          .style('opacity', 0)
+      })
+
+      g.append("g")
+          .attr("class", "axis y-axis")
+          .call(d3.axisLeft(y).tickFormat(PCTFORMAT).tickSize(-width).ticks(num_ticks));
 
 
     // rotate x-axis tick labels on small screens so they don't overlap
@@ -234,13 +258,13 @@ function setupChart(race) {
                 else return "race moe";
             }
         })
-        .attr("x", function(d) { return x(parseTime( d.date_int.split('–')[0] )) })
+        .attr("x", function(d) { return x(d.date_int); })
         .attr("y", function(d) {
             if(isNaN(d.moe_95_ub)) return y(0);
             else if(d.moe_95_ub > 1) return y(1);
             else return y(d.moe_95_ub);
          })
-        .attr("width", function(d) { return myBandwidth ; })
+        .attr("width", function(d) { return x.bandwidth(); ; })
         .attr("height", function(d) {
             if(isNaN(d.moe_95_ub)) return 0;
             else if(d.moe_95_lb < 0 && d.moe_95_ub <= 1) return y(0) - y(d.moe_95_ub);
@@ -281,7 +305,7 @@ function setupChart(race) {
                 else return "race dot";
             }
         })
-        .attr("cx", function(d) { return x(parseTime( d.date_int.split('–')[0] )) + myBandwidth *.5; })
+        .attr("cx", function(d) { return x(d.date_int) + x.bandwidth() *.5; })
         .attr("cy", function(d) { return y(+d.mean); })
         .attr("r", 4)
         .classed("insig", function(d) {
@@ -306,8 +330,8 @@ function setupChart(race) {
         });
 
     // add line demarcating where Phase 2 ends and Phase 3 begins
-    var padding_inner_amount = x("10/28/20–11/9/20") - (x("10/14/20–10/26/20") + myBandwidth );
-    var phase2_end_pos = x("10/14/20–10/26/20") + myBandwidth  + (padding_inner_amount / 2);
+    var padding_inner_amount = x("10/28/20–11/9/20") - (x("10/14/20–10/26/20") + x.bandwidth() );
+    var phase2_end_pos = x("10/14/20–10/26/20") + x.bandwidth();  + (padding_inner_amount / 2);
 
     var phase2_end_line = g.append("g")
         .attr("class", "phase_end_line");
@@ -332,6 +356,47 @@ function setupChart(race) {
         .attr("y", 0)
         .attr("dy", "-0.5em")
         .text("Phase 2 ends");
+
+
+    // add line demarcating where Phase 3 ends and Phase 3.1 begins
+    var phase3_end_pos = x("3/17/21–3/29/21") + x.bandwidth();  + (padding_inner_amount / 2);
+
+    var phase3_end_line = g.append("g")
+        .attr("class", "phase_end_line phase3-1");
+
+    phase3_end_line.append("line")
+        .attr("x1", phase3_end_pos)
+        .attr("x2", phase3_end_pos)
+        .attr("y1", -margin.top)
+        .attr("y2", height)
+        .style("stroke-dasharray", "3 2");
+
+    phase3_end_line.append("text")
+        .attr("class", "phase_begin ")
+        .attr("x", phase3_end_pos + 5)
+        .attr("y", 0)
+        .attr("dy", "-0.5em")
+        .text("Phase 3.1 begins");
+
+    phase3_end_line.append("text")
+        .attr("class", "question-note")
+        .attr("x", phase3_end_pos + 5)
+        .attr("y", 10)
+        .text("Question wording")
+
+    phase3_end_line.append("text")
+        .attr("class", "question-note")
+        .attr("x", phase3_end_pos + 5)
+        .attr("y", 22)
+        .text("changed")
+
+    phase3_end_line.append("text")
+        .attr("class", "phase_end")
+        .attr("x", phase3_end_pos - 5)
+        .attr("y", 0)
+        .attr("dy", "-0.5em")
+        .text("Phase 2 ends");
+
 }
 
 function update() {
@@ -345,6 +410,14 @@ function update() {
     else if(geo_level === "state") geo = getGeography("state");
     else if(geo_level === "msa") geo = getGeography("msa");
     selected_geo = geo;
+
+    // add message about question wording change to inc_loss only
+
+    if (metric === 'inc_loss'){
+      d3.selectAll('.question-note').style('display', 'inline')
+    } else {
+      d3.selectAll('.question-note').style('display', 'none')
+    }
 
     updateChart("national", metric, geo);
     updateChart("asian", metric, geo);
@@ -391,12 +464,12 @@ function updateChart(race, metric, geo) {
                 else return "race moe";
             }
         })
-        .attr("x", function(d) { return x(parseTime( d.date_int.split('–')[0] )) })
+        .attr("x", function(d) { return x(d.date_int); })
         .attr("y", function(d) {
             if(isNaN(d.moe_95_ub)) return y(0);
             else if(d.moe_95_ub > 1) return y(1);
             else return y(d.moe_95_ub); })
-        .attr("width", function(d) { return myBandwidth ; })
+        .attr("width", function(d) { return x.bandwidth(); })
         .attr("height", function(d) {
             if(isNaN(d.moe_95_ub)) return 0;
             else if(d.moe_95_lb < 0 && d.moe_95_ub <= 1) return y(0) - y(d.moe_95_ub);
@@ -435,7 +508,7 @@ function updateChart(race, metric, geo) {
                 else return "race dot";
             }
         })
-        .attr("cx", function(d) { return x(parseTime( d.date_int.split('–')[0] )) + myBandwidth *.5; })
+        .attr("cx", function(d) { return x(d.date_int) + x.bandwidth() *.5; })
         .attr("cy", function(d) { return !isNaN(d.mean) ? y(+d.mean) : y(-1); })
         .classed("insig", function(d) {
             if(d.geography === "dummy data") return false;
@@ -511,3 +584,10 @@ function wrap(text, width) {
         text.append("tspan").attr("x", 0).attr("y", y).attr("dy", lineHeight + dy + "em").text(words.substr(hyphenLocation + 1, words.length))
   });
 }
+
+d3.selection.prototype.moveToFront = function() {
+  return this.each(function(){
+    this.parentNode.appendChild(this);
+  });
+};
+
